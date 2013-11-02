7f19cfad11333eb6d83d8a2395c6d7fb3bbb5347
Benedikt Wolters <benedikt.wolters@googlemail.com>
2013-11-02 22:35:43 +0100

    updated Procfile

--- config.rb
+++ config.rb
@@ -1,3 +1,20 @@
+$stdout.sync = true
+
+use Rack::Static,
+  :urls => ["/css", "/dist", "/images", "/spec"],
+  :root => "."
+
+run lambda { |env|
+  [
+    200,
+    {
+      'Content-Type'  => 'text/html',
+      'Cache-Control' => 'public, max-age=86400'
+    },
+    File.open('index.html', File::RDONLY)
+  ]
+}
+
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
